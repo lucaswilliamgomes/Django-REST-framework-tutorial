874efd3c06f3d67bddcedcc66b0791540e00bf08
Lucas William <lucaswilliam133@gmail.com>
2021-02-03 17:38:48 -0300

    complete tutorial4

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
         
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
