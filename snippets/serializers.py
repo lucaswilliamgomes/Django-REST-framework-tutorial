e1d5d72718ad944abd62e99d9c00d40ef21c7586
Lucas William Gomes Do Amaral <62778691+lucaswilliamgomes@users.noreply.github.com>
2021-02-05 23:27:49 -0300

    Update snippets/serializers.py
    Co-authored-by: Ana Paula Mendes <anapaula.ds.mendes@gmail.com>

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -16,7 +16,6 @@ class SnippetSerializer(serializers.HyperlinkedModelSerializer):
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
-
     class Meta:
         model = User
         fields = ['url', 'id', 'username', 'snippets']
