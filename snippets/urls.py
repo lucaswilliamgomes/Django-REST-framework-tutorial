49f69b82b07df868c12d378f5a37afc523f90e45
Lucas William Gomes Do Amaral <62778691+lucaswilliamgomes@users.noreply.github.com>
2021-02-05 23:28:01 -0300

    Update snippets/urls.py
    Co-authored-by: Ana Paula Mendes <anapaula.ds.mendes@gmail.com>

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from snippets import views
 
-# Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
