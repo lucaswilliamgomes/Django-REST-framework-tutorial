00d7a8df1504297e9c48f4804e76a6d38733e399
Lucas William Gomes Do Amaral <62778691+lucaswilliamgomes@users.noreply.github.com>
2021-02-05 23:27:20 -0300

    Update tutorial/settings.py
    Co-authored-by: Ana Paula Mendes <anapaula.ds.mendes@gmail.com>

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default="changeme")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
